ad2db9430ecabea6d62827e17310824ab0cef107
klgd <slice1213@gmail.com>
2019-01-10 14:51:26 +0800

    find change to findOrFail, and add find

--- src/Prettus/Repository/Eloquent/BaseRepository.php
+++ src/Prettus/Repository/Eloquent/BaseRepository.php
@@ -450,12 +450,30 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
     /**
      * Find data by id
      *
-     * @param       $id
+     * @param mixed $id
      * @param array $columns
-     *
      * @return mixed
+     * @throws \Prettus\Repository\Exceptions\RepositoryException
      */
     public function find($id, $columns = ['*'])
+    {
+        $this->applyCriteria();
+        $this->applyScope();
+        $model = $this->model->find($id, $columns);
+        $this->resetModel();
+
+        return $this->parserResult($model);
+    }
+
+    /**
+     * Find data by id or throw an exception.
+     *
+     * @param mixed $id
+     * @param array $columns
+     * @return mixed
+     * @throws \Prettus\Repository\Exceptions\RepositoryException
+     */
+    public function findOrFail($id, $columns = ['*'])
     {
         $this->applyCriteria();
         $this->applyScope();
